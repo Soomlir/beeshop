93a58c516b4971757b918f2c10eaa1bfd0e2a6ff
Soomlir <Soomlir@yandex.ru>
2024-03-10 17:58:00 +0300

    init commit

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,3 +1,22 @@
 <template>
-	<header></header>
+	<header class="page-header">
+		<div class="page-header__navigation">
+
+		</div>
+	</header>
 </template>
+
+<style>
+	body {
+		min-width: 375px;
+	}
+
+	.page-header {
+		min-height: 154px;
+	}
+
+	.page-header__navigation {
+		min-height: 100px;
+		background: linear-gradient(92.55deg, #f2b236 32.31%, #ff9c1a 64.34%);
+	}
+</style>
