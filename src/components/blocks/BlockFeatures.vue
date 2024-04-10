4b3b401360f03cdc1731b96b21e6f260f89fa47c
Soomlir <Soomlir@yandex.ru>
2024-04-10 23:33:47 +0300

    features list add

--- src/components/blocks/BlockFeatures.vue
+++ src/components/blocks/BlockFeatures.vue
@@ -2,6 +2,20 @@
 	<section class="features">
 		<h2 class="visually-hidden">Наши преимущества</h2>
 		<p class="features__text">Квест – возможность подарить детям незабываемые эмоции!</p>
+		<ul class="features__list">
+			<li class="features__item">
+
+			</li>
+			<li class="features__item">
+
+			</li>
+			<li class="features__item">
+
+			</li>
+			<li class="features__item">
+
+			</li>
+		</ul>
 	</section>
 </template>
 
