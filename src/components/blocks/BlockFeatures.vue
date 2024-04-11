1281437e79062c90b2a6dc0f2737fb03ac33a6e4
Soomlir <Soomlir@yandex.ru>
2024-04-11 21:34:16 +0300

    features list markup add

--- src/components/blocks/BlockFeatures.vue
+++ src/components/blocks/BlockFeatures.vue
@@ -1,19 +1,19 @@
 <template>
 	<section class="features">
 		<h2 class="visually-hidden">Наши преимущества</h2>
-		<p class="features__text">Квест – возможность подарить детям незабываемые эмоции!</p>
+		<p class="features__promo">Квест – возможность подарить детям незабываемые эмоции!</p>
 		<ul class="features__list">
-			<li class="features__item">
-
+			<li class="features__item features__item--1">
+				<p class="features__text">Объединяет семью и&nbsp;друзей в поисках одной разгадки!</p>
 			</li>
-			<li class="features__item">
-
+			<li class="features__item features__item--2">
+				<p class="features__text">Развивает логику и&nbsp;последовательное мышление!</p>
 			</li>
-			<li class="features__item">
-
+			<li class="features__item features__item--3">
+				<p class="features__text">Заменит аниматора на празднике и&nbsp;обеспечит мероприятие на 5+!</p>
 			</li>
-			<li class="features__item">
-
+			<li class="features__item features__item--4">
+				<p class="features__text">Все необходимое - уже есть в коробке!</p>
 			</li>
 		</ul>
 	</section>
@@ -24,9 +24,63 @@
 	padding: 15px 20px;
 }
 
-.features__text {
+.features__promo {
 	margin: 0;
+	margin-bottom: 55px;
 	font-weight: 500;
 	font-size: 32px;
+}
+
+.features__list {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	font-weight: 500;
+	font-size: 20px;
+	line-height: 24px;
+	color: #222528;
+}
+
+.features__item {
+	position: relative;
+	display: flex;
+	align-items: center;
+	min-height: 101px;
+	margin-bottom: 23px;
+	padding-left: 117px;
+
+	&::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 101px;
+		height: 101px;
+		background-color: #ffffff;
+		background-repeat: no-repeat;
+		background-position: 50%;
+		border-radius: 14px;
+		box-shadow: 0 4px 9px #dadada;
+	}
+
+	&--1::before {
+		background-image: url('/images/icons/features-1.svg');
+	}
+
+	&--2::before {
+		background-image: url('/images/icons/features-2.svg');
+	}
+
+	&--3::before {
+		background-image: url('/images/icons/features-3.svg');
+	}
+
+	&--4::before {
+		background-image: url('/images/icons/features-4.svg');
+	}
+}
+
+.features__text {
+	margin: 0;
 }
 </style>
