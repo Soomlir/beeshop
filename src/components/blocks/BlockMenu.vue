717f7bbe10a226622ea5347de4fce51b94893691
Soomlir <Soomlir@yandex.ru>
2024-07-04 22:21:15 +0300

    mobile menu edits

--- src/components/blocks/BlockMenu.vue
+++ src/components/blocks/BlockMenu.vue
@@ -1,13 +1,11 @@
 <script setup>
-defineEmits(
-	['closeMenu'],
-);
+defineEmits(['closeMenu']);
 </script>
 
 <template>
 	<div class="menu">
 		<div class="menu__cabinet">
-			<button  @click="$emit('closeMenu')" class="menu__close">
+			<button @click="$emit('closeMenu')" class="menu__close">
 				<span class="visually-hidden">Закрыть меню.</span>
 			</button>
 			<div class="menu__info">
@@ -24,26 +22,40 @@ defineEmits(
 						<a class="menu__link menu__link--current" href="#!">Каталог</a>
 					</li>
 					<li class="menu__item">
-						<a class="menu__link" href="#!">Акции и новости</a>
+						<a class="menu__link" href="#!"
+							>Акции и новости<span class="menu__link--bullet"></span></a
+						>
 					</li>
 					<li class="menu__item"><a class="menu__link" href="#!">Отзывы</a></li>
 					<li class="menu__item">
 						<a class="menu__link" href="#!">Доставка и оплата</a>
 					</li>
 					<li class="menu__item">
-						<a class="menu__link" href="#!">О компании</a>
-						<ul>
+						<a class="menu__link menu__link--arrow" href="#!">О компании</a>
+						<ul class="menu__list-inner">
 							<li><a class="menu__link-inner" href="#!">О нас</a></li>
 							<li><a class="menu__link-inner" href="#!">Контакты</a></li>
 							<li><a class="menu__link-inner" href="#!">Документы</a></li>
 						</ul>
 					</li>
 				</ul>
-				<ul>
-					<li><a href="#!">Vk</a></li>
-					<li><a href="#!">Instagram</a></li>
+				<ul class="menu__icon-list">
+					<li class="menu__icon-item">
+						<a class="menu__icon menu__icon--vk" href="#!"
+							><span class="visually-hidden">Vk</span></a
+						>
+					</li>
+					<li class="menu__icon-item">
+						<a class="menu__icon menu__icon--insta" href="#!"
+							><span class="visually-hidden">Instagram</span></a
+						>
+					</li>
 				</ul>
-				<input type="search" placeholder="Поиск по сайту..." />
+				<input
+					class="menu__search"
+					type="search"
+					placeholder="Поиск по сайту..."
+				/>
 			</nav>
 		</div>
 	</div>
@@ -55,14 +67,18 @@ defineEmits(
 
 	&--active {
 		position: absolute;
-		top: 0;
-		right: 0;
-		left: 0;
 		z-index: 100;
 		display: flex;
 		width: 375px;
-		height: 1131px;
+		overflow-y: auto;
 		background-color: rgba(34, 37, 40, 0.79);
+		box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+		inset: 0;
+		backdrop-filter: blur(10px);
+
+		@media (min-width: 768px) {
+			width: 768px;
+		}
 	}
 }
 
@@ -70,6 +86,11 @@ defineEmits(
 	display: grid;
 	grid-template-columns: 1fr 1fr;
 	grid-template-rows: 85px 100px;
+
+	@media (min-width: 768px) {
+		grid-template-rows: 173px 100px;
+		width: 100%;
+	}
 }
 
 .menu__close {
@@ -86,14 +107,28 @@ defineEmits(
 	border: none;
 	border-radius: 8px;
 	cursor: pointer;
+
+	@media (min-width: 768px) {
+		grid-column: 2 / -1;
+		width: 66px;
+		height: 66px;
+		margin-top: 42px;
+		margin-right: 54px;
+		background-image: url('/images/icons/crest-tablet.svg');
+	}
 }
 
 .menu__info {
 	display: grid;
 	grid-column: 1 / -1;
-	grid-column: span 2;
 	grid-template-columns: 40px 1fr;
+	grid-template-rows: 40px 40px;
 	padding: 11px 22px;
+
+	@media (min-width: 768px) {
+		grid-template-columns: 66px 1fr 1fr;
+		padding: 13px 53px;
+	}
 }
 
 .menu__portret {
@@ -104,6 +139,12 @@ defineEmits(
 	background-repeat: no-repeat;
 	background-position: 50% 50%;
 	border-radius: 8px;
+
+	@media (min-width: 768px) {
+		width: 66px;
+		height: 66px;
+		background-image: url('/images/icons/menu-user-tablet.svg');
+	}
 }
 
 .menu__heading {
@@ -113,27 +154,66 @@ defineEmits(
 	font-size: 20px;
 	line-height: 24px;
 	color: $darken-white-color;
+
+	@media (min-width: 768px) {
+		grid-column: 2 / 3;
+		width: 200px;
+		margin-top: 11px;
+		margin-left: 10px;
+		font-size: 24px;
+	}
 }
 
 .menu__options {
 	display: flex;
 	grid-column: 2 / -1;
+
+	@media (min-width: 768px) {
+		grid-column: 3 / -1;
+	}
 }
 
 .menu__reg,
 .menu__auth {
 	display: inline-block;
 	align-self: start;
-	padding: 0 15px;
+	padding: 0 16px;
+	padding-top: 9px;
 	font-size: 16px;
 	line-height: 24px;
 	color: $base-white-color;
 	text-decoration: none;
 }
 
+.menu__reg {
+	@media (min-width: 768px) {
+		margin-top: 19px;
+		margin-left: 56px;
+	}
+}
+
+.menu__auth {
+	@media (min-width: 768px) {
+		margin-top: 19px;
+		margin-left: 33px;
+		padding-right: 14px;
+	}
+}
+
 .menu__nav {
-	min-width: 375px;
-	margin-top: 35px;
+	box-sizing: border-box;
+	min-width: 335px;
+	margin-top: 36px;
+	margin-right: 20px;
+	margin-left: 20px;
+
+	@media (min-width: 768px) {
+		width: 100%;
+		width: 660px;
+		margin-top: 62px;
+		margin-right: 54px;
+		margin-left: 54px;
+	}
 }
 
 .menu__list {
@@ -143,26 +223,112 @@ defineEmits(
 	list-style: none;
 }
 
+.menu__item {
+	border-bottom: 1px solid #a5aaaf;
+}
+
 .menu__link {
+	position: relative;
 	display: flex;
 	padding: 24px 22px;
-	font-size: 21px;
+	padding-left: 0;
+	font-weight: 500;
+	font-size: 20px;
 	line-height: 24px;
 	color: $base-white-color;
 	text-decoration: none;
 
-	&--current {
-		background-color: #f2b236;
+	&--bullet {
+		position: absolute;
+		right: 140px;
+		bottom: 6px;
+		width: 15px;
+		height: 15px;
+		background-color: #db4e66;
+		border-radius: 50%;
 	}
+
+	&--arrow {
+		background-image: url('/images/icons/arrow-menu-mobile.svg');
+		background-repeat: no-repeat;
+		background-position: 303px 50%;
+	}
+
+	&--current {
+		margin-right: -20px;
+		margin-left: -20px;
+		padding-left: 20px;
+		background-color: #f2b236;
+
+		@media (min-width: 768px) {
+			margin-right: -54px;
+			margin-left: -54px;
+		}
+	}
+}
+
+.menu__list-inner {
+	list-style: none;
 }
 
 .menu__link-inner {
 	display: flex;
 	padding-bottom: 24px;
-	padding-left: 22px;
-	font-size: 21px;
+	font-weight: 500;
+	font-size: 20px;
 	line-height: 24px;
 	color: $base-white-color;
 	text-decoration: none;
+}
+
+.menu__icon-list {
+	display: flex;
+	margin: 0;
+	padding: 0;
+	padding: 36px 22px;
+	padding-bottom: 20px;
+	padding-left: 0;
+	list-style: none;
+}
+
+.menu__icon-item {
+	margin-right: 16px;
+}
+
+.menu__icon {
+	display: flex;
+	width: 52px;
+	height: 52px;
+	background-color: #90c1bf;
+	border-radius: 8px;
+
+	&--vk {
+		background-image: url('/images/icons/vk.svg');
+		background-repeat: no-repeat;
+		background-position: 50% 50%;
+	}
+
+	&--insta {
+		background-image: url('/images/icons/insta.svg');
+		background-repeat: no-repeat;
+		background-position: 50% 50%;
+	}
+}
+
+.menu__search {
+	width: 333px;
+	height: 56px;
+	margin: 20px 20px 201px 0;
+	margin-bottom: 70px;
+	padding: 10px 0;
+	padding-right: 50px;
+	padding-left: 22px;
+	font-size: 16px;
+	color: #a5aaaf;
+	background-image: url('/images/icons/search-input.svg');
+	background-repeat: no-repeat;
+	background-position: 288px 50%;
+	border: none;
+	border-radius: 30px;
 }
 </style>
