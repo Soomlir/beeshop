ad73ef8ccc8f2f5178e5983578e11614f5fc2ac1
Soomlir <Soomlir@yandex.ru>
2024-10-04 21:22:46 +0300

    cooperation block add

--- src/components/blocks/BlockCooperation.vue
+++ src/components/blocks/BlockCooperation.vue
@@ -55,7 +55,8 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 			</li>
 			<li class="cooperation__item">
 				<p class="cooperation__heading-inner">
-					Вам нравится наш продукт? Расскажите о нём и получите<br />
+					Вам нравится наш продукт?<br class="tablet" />
+					Расскажите о нём и получите<br class="mobile" />
 					его бесплатно!
 				</p>
 				<p class="cooperation__paragraph">Сотрудничество для блогеров</p>
@@ -101,7 +102,9 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 				<p class="cooperation__features">
 					Для магазинов, сетей, настольных клубов, СП
 				</p>
-				<p class="cooperation__list-title">Преимущества сотрудничества с нами:</p>
+				<p class="cooperation__list-title">
+					Преимущества сотрудничества с нами:
+				</p>
 				<ul class="cooperation__list-features">
 					<li class="cooperation__item-features">
 						Приобретение товаров напрямую у производителя. Мы несем
@@ -142,8 +145,8 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 		</ul>
 
 		<p class="cooperation__accent">
-			<span>Начните продавать</span> трендовые игры уже завтра, пока до нас не добрались Ваши
-			конкуренты!
+			<span>Начните продавать</span> трендовые игры уже завтра, пока до нас не
+			добрались Ваши конкуренты!
 		</p>
 	</section>
 	<section class="feedback-form">
@@ -158,37 +161,41 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 		<form class="feedback__form" action="/">
 			<label>
 				<p>Фамилия<span>*</span></p>
-				<input type="text" name="lastName" placeholder="Иванова">
+				<input type="text" name="lastName" placeholder="Иванова" />
 			</label>
 			<label>
 				<p>Имя<span>*</span></p>
-				<input type="text" name="name" placeholder="Алена">
+				<input type="text" name="name" placeholder="Алена" />
 			</label>
 			<label>
 				<p>Отчество<span>*</span></p>
-				<input type="text" name="name2" placeholder="Николаевна">
+				<input type="text" name="name2" placeholder="Николаевна" />
 			</label>
 			<label>
 				<p>Наименование юр.лица<span>*</span></p>
-				<input type="text" name="lawyer">
+				<input type="text" name="lawyer" />
 			</label>
 			<label>
 				<p>Номер телефона<span>*</span></p>
-				<input type="tel" name="tel" placeholder="+7 -">
+				<input type="tel" name="tel" placeholder="+7 -" />
 			</label>
 			<label>
 				<p>E-mail<span>*</span></p>
-				<input type="email" name="email">
+				<input type="email" name="email" />
 			</label>
-			<p>Способ реализации продукции<span>*</span></p>
-			<select class="feedback__form-select">
-				<option>Розничный магазин</option>
-				<option>Розничная сеть</option>
-				<option>Интернет-магазин</option>
-				<option>СП</option>
-				<option>Другое</option>
-			</select>
-			<button type="submit">Оставить заявку</button>
+			<div>
+				<p>Способ реализации продукции<span>*</span></p>
+				<select class="feedback__form-select">
+					<option>Розничный магазин</option>
+					<option>Розничная сеть</option>
+					<option>Интернет-магазин</option>
+					<option>СП</option>
+					<option>Другое</option>
+				</select>
+			</div>
+			<button class="feedback__form-submit" type="submit">
+				Оставить заявку
+			</button>
 		</form>
 	</section>
 </template>
@@ -197,6 +204,11 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 .cooperation {
 	margin-bottom: 28px;
 	padding: 55px 20px;
+
+	@media (min-width: 768px) {
+		padding: 80px 54px;
+		padding-bottom: 16px;
+	}
 }
 
 .cooperation__title {
@@ -205,6 +217,10 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	font-weight: 500;
 	font-size: 28px;
 	color: #222528;
+
+	@media (min-width: 768px) {
+		font-size: 32px;
+	}
 }
 
 .cooperation__info {
@@ -213,17 +229,31 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	font-weight: 500;
 	font-size: 16px;
 	color: #5b636b;
+
+	@media (min-width: 768px) {
+		margin-bottom: 64px;
+		font-size: 20px;
+	}
 }
 
 .cooperation__heading {
 	margin: 0;
 	margin-bottom: 25px;
+
+	@media (min-width: 768px) {
+		margin-bottom: 42px;
+		padding-left: 43px;
+	}
 }
 
 .cooperation__text {
 	margin: 0;
 	margin-bottom: 16px;
 	font-size: 16px;
+
+	@media (min-width: 768px) {
+		margin-bottom: 22px;
+	}
 }
 
 .cooperation__list {
@@ -244,6 +274,10 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	padding: 0;
 	padding-left: 57px;
 	list-style: circle;
+
+	@media (min-width: 768px) {
+		margin-bottom: 32px;
+	}
 }
 
 .features__item {
@@ -255,6 +289,10 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 
 .cooperation__contacts {
 	margin-bottom: 64px;
+
+	@media (min-width: 768px) {
+		margin-bottom: 66px;
+	}
 }
 
 .cooperation__contact-text {
@@ -274,6 +312,18 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 .cooperation__heading-inner {
 	margin: 0;
 	margin-bottom: 8px;
+
+	@media (min-width: 768px) {
+		padding-left: 43px;
+
+		& .mobile {
+			display: none;
+		}
+
+		& .tablet {
+			display: block;
+		}
+	}
 }
 
 .cooperation__paragraph {
@@ -281,6 +331,11 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	margin-bottom: 24px;
 	font-size: 16px;
 	color: #5b636b;
+
+	@media (min-width: 768px) {
+		margin-bottom: 40px;
+		margin-left: 43px;
+	}
 }
 
 .cooperation__steps {
@@ -288,6 +343,10 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	margin-bottom: 15px;
 	font-size: 16px;
 	color: #222528;
+
+	@media (min-width: 768px) {
+		margin-bottom: 24px;
+	}
 }
 
 .algorithm__list {
@@ -298,6 +357,10 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	font-size: 16px;
 	line-height: 26px;
 	color: #222528;
+
+	@media (min-width: 768px) {
+		margin-bottom: 32px;
+	}
 }
 
 .cooperation__order {
@@ -314,6 +377,10 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 .cooperation__partner {
 	margin: 0;
 	margin-bottom: 9px;
+
+	@media (min-width: 768px) {
+		padding-left: 43px;
+	}
 }
 
 .cooperation__features {
@@ -321,6 +388,11 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	margin-bottom: 21px;
 	font-size: 16px;
 	color: #5b636b;
+
+	@media (min-width: 768px) {
+		margin-bottom: 37px;
+		padding-left: 43px;
+	}
 }
 
 .cooperation__list-title {
@@ -355,6 +427,12 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	font-weight: 500;
 	color: #fafaf9;
 	background: linear-gradient(92.55deg, #f2b236 32.31%, #ff9c1a 64.34%);
+
+	@media (min-width: 768px) {
+		margin-bottom: 120px;
+		padding-top: 67px;
+		padding-bottom: 0;
+	}
 }
 
 .feedback-form__title {
@@ -374,6 +452,8 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 }
 
 .feedback__form {
+	margin-bottom: 120px;
+	padding-bottom: 64px;
 	font-weight: 400;
 
 	& label {
@@ -403,10 +483,22 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 			color: #ced0d2;
 		}
 	}
+
+	@media (min-width: 768px) {
+		display: grid;
+		grid-auto-flow: column;
+		grid-template-columns: 315px 315px;
+		grid-template-rows: 56px 56px 56px 56px;
+		margin-bottom: 0;
+		padding: 24px 25px;
+		padding-bottom: 0;
+		gap: 30px;
+	}
 }
 
 .feedback__form-select {
 	width: 100%;
+	margin-bottom: 32px;
 	padding: 11px 10px;
 	font-size: 14px;
 	background: #f9f9f9;
@@ -416,6 +508,26 @@ import SocialLink from '@/components/blocks/SocialLink.vue';
 	&::placeholder {
 		font-size: 14px;
 		color: #ced0d2;
+	}
+}
+
+.feedback__form-submit {
+	width: 276px;
+	margin-left: 18px;
+	padding: 18px 20px;
+	font-weight: 500;
+	font-size: 15px;
+	color: #fafaf9;
+	text-transform: uppercase;
+	background-color: #db4e66;
+	border: none;
+	border-radius: 13px;
+
+	@media (min-width: 768px) {
+		grid-column: 1/-1;
+		grid-row: 5/6;
+		margin-top: 30px;
+		margin-left: 190px;
 	}
 }
 </style>
--- src/components/blocks/SocialLink.vue
+++ src/components/blocks/SocialLink.vue
@@ -7,12 +7,17 @@
 		</a>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .social {
 	display: block;
 	width: 42px;
 	height: 42px;
 	background-color: #90c1bf;
 	border-radius: 4px;
+
+	@media (min-width: 768px) {
+		width: 53px;
+		height: 53px;
+	}
 }
 </style>
