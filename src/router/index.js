10f5661818bf76c89eaba2aaa01d71ff9956f8ed
Soomlir <Soomlir@yandex.ru>
2024-10-05 18:29:30 +0300

    order page add

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import IndexPage from '@/router/IndexPage.vue';
 import CooperationPage from '@/router/CooperationPage.vue';
+import OrderPage from '@/router/OrderPage.vue';
 import NotFoundPage from '@/router/NotFoundPage.vue';
 
 const routes = [
@@ -13,6 +14,11 @@ const routes = [
 		path: '/cooperation',
 		name: 'cooperation',
 		component: CooperationPage,
+	},
+	{
+		path: '/order',
+		name: 'order',
+		component: OrderPage,
 	},
 ];
 
